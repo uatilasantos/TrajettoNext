9600f8b0c4d878e45427184f4c23c5568623f4f3
Ana Santos <ana1908beatrozsantos@gmail.com>
2025-11-16 21:49:29 -0300

    Fix: corrigindo Id de Usuario

--- src/app/(protected)/dashboard/carga/page.js
+++ src/app/(protected)/dashboard/carga/page.js
@@ -16,7 +16,8 @@ export default function CargasPage() {
   const [veiculos, setVeiculos] = useState([]);
   const [motoristas, setMotoristas] = useState([]);
   const [cidadesSP, setCidadesSP] = useState([]);
-    const [form, setForm] = useState({
+  const [usuarioId, setUsuarioId] = useState([]);
+  const [form, setForm] = useState({
     tipo_carga: "",
     peso_carga: "",
     motorista_id: "",
@@ -26,12 +27,13 @@ export default function CargasPage() {
     destino_carga: "",
     valor_frete: "",
     valor_km: "",
-    distancia: ""
+    distancia: "",
+    usuario_id: "" 
   });
 
   const [visualizando, setVisualizando] = useState(null);
   const [mostrarPopup, setMostrarPopup] = useState(false);
-  
+
 
 
   useEffect(() => {
@@ -64,7 +66,7 @@ export default function CargasPage() {
 
   async function carregarVeiculos() {
     try {
-      const resposta = await fetch(apiUrlVeiculos); 
+      const resposta = await fetch(apiUrlVeiculos);
       const data = await resposta.json();
       setVeiculos(data);
     } catch (error) {
@@ -83,20 +85,20 @@ export default function CargasPage() {
     }
   }
 
-async function carregarCidadesSP() {
+  async function carregarCidadesSP() {
     const apiIbgeUrl = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/35/municipios";
 
     try {
-        const response = await fetch(apiIbgeUrl);
-        let cidades = await response.json();
-        cidades.sort((a, b) => a.nome.localeCompare(b.nome));
+      const response = await fetch(apiIbgeUrl);
+      let cidades = await response.json();
+      cidades.sort((a, b) => a.nome.localeCompare(b.nome));
 
-        setCidadesSP(cidades);
+      setCidadesSP(cidades);
 
     } catch (error) {
-        console.error("Erro ao carregar cidades:", error);
+      console.error("Erro ao carregar cidades:", error);
     }
-}
+  }
 
   function handleChange(e) {
     const { name, value } = e.target;
@@ -129,7 +131,7 @@ async function carregarCidadesSP() {
     } catch (error) {
       console.error("Erro ao salvar carga:", error);
     }
-}
+  }
 
 
 
@@ -159,15 +161,15 @@ async function carregarCidadesSP() {
   }
 
   async function DetalhesCarga(id) {
-  try {
-    const response = await fetch(`${apiUrl}/${id}`);
-    const data = await response.json();
-    setVisualizando(data);
-    setMostrarPopup(true);
-  } catch (error) {
-    console.error("Erro ao visualizar carga:", error);
+    try {
+      const response = await fetch(`${apiUrl}/${id}`);
+      const data = await response.json();
+      setVisualizando(data);
+      setMostrarPopup(true);
+    } catch (error) {
+      console.error("Erro ao visualizar carga:", error);
+    }
   }
-}
 
 
   function limparFormulario() {
@@ -191,35 +193,43 @@ async function carregarCidadesSP() {
       <div className={styles.container}>
         <h1>Manifesto de Carga</h1>
         <form onSubmit={handleSubmit} className={styles.formCarga}>
-          <h3>Dados da Carga</h3>   
-          <select name="cliente_id" value={form.cliente_id} onChange={handleChange} required> <option value="">Selecione um cliente</option> {clientes.map((c) => ( <option key={c.id} value={c.id}> {c.id} - {c.razao_social} </option>))} </select>          
-          <select name="tipo_carga" value={form.tipo_carga} onChange={handleChange} required > <option value="">Selecione o tipo de carga</option> <option value="seca">Seca</option> <option value="refrigerada">Refrigerada</option> </select>          
+          <h3>Dados da Carga</h3>
+          <select name="cliente_id" value={form.cliente_id} onChange={handleChange} required> <option value="">Selecione um cliente</option> {clientes.map((c) => (<option key={c.id} value={c.id}> {c.id} - {c.razao_social} </option>))} </select>
+          <select name="tipo_carga" value={form.tipo_carga} onChange={handleChange} required > <option value="">Selecione o tipo de carga</option> <option value="seca">Seca</option> <option value="refrigerada">Refrigerada</option> </select>
           <input name="peso_carga" placeholder="Peso" value={form.peso_carga} onChange={handleChange} required />
-          <select name="motorista_id" value={form.motorista_id} onChange={handleChange}> <option value="">Selecione um motorista</option> {motoristas.map((m) => (<option key={m.id} value={m.id}> {m.id} - {m.nome} </option> ))} </select>          
-          <select name="veiculo_id" value={form.veiculo_id} onChange={handleChange}> <option value="">Selecione um veículo</option> {veiculos.map((v) => (<option key={v.id} value={v.id}> {v.id} - {v.placa} - {v.peso_maximo_kg}kg </option> ))} </select>          
+          <select name="motorista_id" value={form.motorista_id} onChange={handleChange}> <option value="">Selecione um motorista</option> {motoristas.map((m) => (<option key={m.id} value={m.id}> {m.id} - {m.nome} </option>))} </select>
+          <select name="veiculo_id" value={form.veiculo_id} onChange={handleChange}> <option value="">Selecione um veículo</option> {veiculos.map((v) => (<option key={v.id} value={v.id}> {v.id} - {v.placa} - {v.peso_maximo_kg}kg </option>))} </select>
           <h3>Dados do Frete</h3>
-          <select name="origem_carga" value={form.origem_carga} onChange={handleChange} required> <option value="">Selecione uma cidade de origem</option> {cidadesSP.map((c) => (<option key={c.id} value={c.nome}>{c.nome}</option> ))}</select>
-          <select name="destino_carga" value={form.destino_carga} onChange={handleChange} required> <option value="">Selecione uma cidade de destino</option> {cidadesSP.map((c) => ( <option key={c.id} value={c.nome}>{c.nome}</option> ))} </select>
+          <select name="origem_carga" value={form.origem_carga} onChange={handleChange} required> <option value="">Selecione uma cidade de origem</option> {cidadesSP.map((c) => (<option key={c.id} value={c.nome}>{c.nome}</option>))}</select>
+          <select name="destino_carga" value={form.destino_carga} onChange={handleChange} required> <option value="">Selecione uma cidade de destino</option> {cidadesSP.map((c) => (<option key={c.id} value={c.nome}>{c.nome}</option>))} </select>
+          <input
+            name="usuario_id"
+            placeholder="Usuário"
+            value={form.usuario_id}
+            onChange={handleChange}
+            required
+          />
+
           <button id="btn-salvar" type="submit">
             {editando ? "Salvar Alterações" : "Cadastrar carga"}
           </button>
         </form>
 
-          {mostrarPopup && visualizando && (
-            <div className={styles.popupOverlay}>
-              <div className={styles.popupContent}>
-                <h3>Detalhes da Carga</h3>
-                <ul>
-                  {Object.entries(visualizando).map(([key, value]) => (
-                    <li key={key}>
-                      <strong>{key.replace("_", " ")}:</strong> {value || "—"}
-                    </li>
-                  ))}
-                </ul>
-                <button className={styles.btnFechar} onClick={() => setMostrarPopup(false)}>Fechar</button>
-              </div>
+        {mostrarPopup && visualizando && (
+          <div className={styles.popupOverlay}>
+            <div className={styles.popupContent}>
+              <h3>Detalhes da Carga</h3>
+              <ul>
+                {Object.entries(visualizando).map(([key, value]) => (
+                  <li key={key}>
+                    <strong>{key.replace("_", " ")}:</strong> {value || "—"}
+                  </li>
+                ))}
+              </ul>
+              <button className={styles.btnFechar} onClick={() => setMostrarPopup(false)}>Fechar</button>
             </div>
-          )}
+          </div>
+        )}
 
       </div>
 
